2ddbe4e4a4c7443bf54157d4b1ead550659c27a4
gerardambe <gerardambe@yahoo.com>
2022-05-27 21:30:43 +0000

    adding comment section

--- letter/models.py
+++ letter/models.py
@@ -45,3 +45,19 @@ class News(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    news = models.ForeignKey(News,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return f'Comment by {self.name} on {self.news}'
